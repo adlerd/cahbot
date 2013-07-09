ef0a69e4c5fdcf1b69665bc09bc5fbce4c7b0254
David Adler <d.adler.s@gmail.com>
2013-07-09 11:29:03 -0400

    Initial.

---
+++ cahbot.go
@@ -0,0 +1,43 @@
+package main
+
+import irc "github.com/fluffle/goirc/client"
+import "fmt"
+import "cahbot"
+import "time"
+
+func main() {
+	var remove irc.Remover
+	// Or, create a config and fiddle with it first:
+	cfg := irc.NewConfig(cahbot.IrcNick)
+	cfg.SSL = true
+	cfg.Server = "irc.freenode.net:7000"
+	cfg.NewNick = func(n string) string { cahbot.IrcNick = n + "^"; return cahbot.IrcNick }
+	cfg.Recover = func(c *irc.Conn, l *irc.Line) {
+		if err := recover(); err != nil {
+			remove.Remove()
+			c.Quit("Panic!")
+			time.Sleep(time.Second)
+			panic(err)
+		}
+	}
+	c := irc.Client(cfg)
+
+	// Add handlers to do things here!
+	// e.g. join a channel on connect.
+	c.HandleFunc("connected",
+		func(conn *irc.Conn, line *irc.Line) { conn.Join(cahbot.IrcChannel) })
+	// And a signal on disconnect
+	quit := make(chan bool)
+	remove = c.HandleFunc("disconnected",
+		func(conn *irc.Conn, line *irc.Line) { quit <- true })
+
+	c.HandleFunc("PRIVMSG", cahbot.HandlePrivMsg)
+
+	// Tell client to connect.
+	if err := c.Connect(); err != nil {
+		fmt.Printf("Connection error: %s\n", err.Error())
+	}
+
+	// Wait for disconnect
+	<-quit
+}
---
+++ cahbot/game.go
@@ -0,0 +1,208 @@
+package cahbot
+
+import irc "github.com/fluffle/goirc/client"
+import "container/ring"
+import "strings"
+import "fmt"
+
+const HAND_SIZE = 7
+
+type User struct {
+	Nick   string
+	Active bool
+	Joined bool
+	Hand   [HAND_SIZE]*WhiteCard
+}
+
+var (
+	activeUsers  *ring.Ring
+	joiningUsers *ring.Ring
+	users        map[string]*User = make(map[string]*User)
+	IrcChannel   string           = "##multiuse"
+	IrcNick      string           = "cahbot"
+	playedCards  []*WhiteCard
+	lenLessOne   int
+)
+
+func MsgPrintF(conn *irc.Conn, target string, format string, a ...interface{}) {
+	conn.Privmsg(target, fmt.Sprintf(format, a...))
+}
+
+func fillHand(user *User, conn *irc.Conn) {
+	var drawn [HAND_SIZE]*WhiteCard
+	for i, c := range user.Hand {
+		if c == nil {
+			card := drawCard()
+			user.Hand[i] = card
+			drawn[i] = card
+		}
+	}
+	conn.Privmsg(user.Nick, cardsList(drawn[:], "You drew"))
+}
+
+func cardsList(cards []*WhiteCard, msgprefix string) (msg string) {
+	for i, card := range cards {
+		if card != nil {
+			msg += fmt.Sprintf("%s (%c) \"%s\"", msgprefix, int('a')+i, card.Text)
+			msgprefix = ";"
+		}
+	}
+	return
+}
+
+func tellHand(nick string, user *User, rest string, conn *irc.Conn) {
+	conn.Privmsg(user.Nick, cardsList(user.Hand[:], "You have"))
+}
+
+func userJoin(nick string, user *User, rest string, conn *irc.Conn) {
+	if user == nil {
+		user = new(User)
+		user.Nick = nick
+		users[nick] = user
+	} else if user.Joined {
+		conn.Privmsg(nick, "Already joined!")
+		return
+	}
+	user.Joined = true
+	newRing := ring.New(1)
+	newRing.Value = user
+	joiningUsers = newRing.Link(joiningUsers)
+	conn.Privmsg(nick, "Succesfully joined. Not yet active.")
+	MsgPrintF(conn, IrcChannel, "%s joined.", nick)
+	if activeUsers == nil {
+		nextRound(conn)
+	}
+}
+
+func nextRound(conn *irc.Conn) {
+	if activeUsers.Len()+joiningUsers.Len() < 2 {
+		conn.Privmsg(IrcChannel, "Not enough users to start.")
+		return
+	}
+	if joiningUsers != nil {
+		joiningUsers.Do(func(v interface{}) {
+			user := v.(*User)
+			user.Active = true
+			MsgPrintF(conn, IrcChannel, "%s now active!", user.Nick)
+			fillHand(user, conn)
+		})
+		if activeUsers == nil {
+			activeUsers = joiningUsers
+		} else {
+			activeUsers = activeUsers.Link(joiningUsers)
+		}
+		joiningUsers = nil
+	} else {
+		activeUsers = activeUsers.Next()
+	}
+	leader := activeUsers.Value.(*User)
+	MsgPrintF(conn, IrcChannel, "%s is the leader!", leader.Nick)
+	lenLessOne = activeUsers.Len() - 1
+	playedCards = make([]*WhiteCard, 0, lenLessOne)
+}
+
+func leaderChoose(nick string, user *User, rest string, conn *irc.Conn) {
+	if user != activeUsers.Value {
+		conn.Privmsg(nick, "You're not the leader!")
+		return
+	}
+	if len(playedCards) != lenLessOne {
+		conn.Privmsg(nick, "Not all the cards are in!")
+		return
+	}
+	err, card, _ := namedCard(playedCards, rest)
+	if err != "" {
+		conn.Privmsg(nick, err)
+		return
+	}
+	MsgPrintF(conn, IrcChannel, "%s has chosen \"%s\"!", nick, card.Text)
+	for _, c := range playedCards {
+		c.Holder.Hand[c.Index] = nil
+		fillHand(c.Holder, conn)
+		discardCard(c)
+	}
+	nextRound(conn)
+}
+
+func namedCard(cards []*WhiteCard, rest string) (string, *WhiteCard, int) {
+	spl := strings.Fields(rest)
+	if len(spl) != 1 || len(spl[0]) != 1 {
+		return "Could not parse. Give me a single letter plx.", nil, 0
+	}
+	c := int(spl[0][0]) - int('a')
+	if c < 0 || c >= len(cards) {
+		return "That's not a letter naming a card...", nil, 0
+	}
+	return "", cards[c], c
+}
+
+func selectCard(nick string, user *User, rest string, conn *irc.Conn) {
+	if user == activeUsers.Value {
+		conn.Privmsg(nick, "You're the leader, use 'choose' when your time comes.")
+		return
+	}
+	if len(playedCards) == lenLessOne {
+		conn.Privmsg(nick, "Cards are locked for choosing.")
+		return
+	}
+	err, card, index := namedCard(user.Hand[:], rest)
+	if err != "" {
+		conn.Privmsg(nick, err)
+		return
+	}
+	card.Holder = user
+	card.Index = index
+	playedCards = append(playedCards, card)
+	didPlay := make([]*WhiteCard, HAND_SIZE)
+	didPlay[index] = card
+	conn.Privmsg(nick, cardsList(didPlay, "You played"))
+	if len(playedCards) == lenLessOne {
+		conn.Privmsg(IrcChannel, cardsList(playedCards, "All cards are in: "))
+		conn.Privmsg(activeUsers.Value.(*User).Nick, "Time to choose.")
+	}
+}
+
+type CommandFunc func(nick string, user *User, rest string, conn *irc.Conn)
+type Command struct {
+	Name          string
+	Function      CommandFunc
+	AllowInactive bool
+}
+
+func NewCommand(name string, function CommandFunc, allowInactive bool) *Command {
+	c := new(Command)
+	c.Name = name
+	c.Function = function
+	c.AllowInactive = allowInactive
+	return c
+}
+
+var Commands []*Command = []*Command{
+	NewCommand("join", userJoin, true),
+	NewCommand("choose", leaderChoose, false),
+	NewCommand("hand", tellHand, false),
+	NewCommand("select", selectCard, false)}
+
+func HandlePrivMsg(conn *irc.Conn, line *irc.Line) {
+	if line.Args[0] != IrcNick {
+		return
+	}
+	spl := strings.SplitN(line.Args[1], " ", 2)
+	rest := ""
+	if len(spl) > 1 {
+		rest = spl[1]
+	}
+	nick := line.Nick
+	user := users[nick]
+	for _, c := range Commands {
+		if strings.HasPrefix(c.Name, spl[0]) {
+			if (user != nil && user.Active) || c.AllowInactive {
+				c.Function(nick, user, rest, conn)
+			} else {
+				conn.Privmsg(nick, "You must be active (implying join) first!")
+			}
+			return
+		}
+	}
+	conn.Privmsg(nick, "Unrecognized command")
+}
